a8a43a945c3d27f827a48007ea995dd6b3bdbdd2
tamanchichan <tamanchichan@gmail.com>
2023-03-16 16:16:05 -0500

    input.value append to div

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,9 +7,10 @@ const contacts = document.querySelector('.contacts');
 button.addEventListener('click', () => {
   function addContact() {
     const box = document.createElement('div');
-    
-    contacts.appendChild(box).classList.add('box');
+    let contact = input.value
+    contacts.appendChild(box, contact).classList.add('box');
     box.style.cursor = 'pointer';
+    box.innerText = input.value;
   }
   
   addContact();

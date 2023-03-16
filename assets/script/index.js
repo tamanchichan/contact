e293fad79dce383f9498566d48b5665a2fe9601b
tamanchichan <tamanchichan@gmail.com>
2023-03-16 16:19:12 -0500

    function outside button

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,14 +4,22 @@ const input = document.querySelector('.input-field');
 const button = document.querySelector('.button');
 const contacts = document.querySelector('.contacts');
 
+function addContact() {
+  const box = document.createElement('div');
+  let contact = input.value
+  contacts.appendChild(box, contact).classList.add('box');
+  box.style.cursor = 'pointer';
+  box.innerText = input.value;
+}
+
 button.addEventListener('click', () => {
-  function addContact() {
-    const box = document.createElement('div');
-    let contact = input.value
-    contacts.appendChild(box, contact).classList.add('box');
-    box.style.cursor = 'pointer';
-    box.innerText = input.value;
-  }
+  // function addContact() {
+  //   const box = document.createElement('div');
+  //   let contact = input.value
+  //   contacts.appendChild(box, contact).classList.add('box');
+  //   box.style.cursor = 'pointer';
+  //   box.innerText = input.value;
+  // }
   
   addContact();
 })

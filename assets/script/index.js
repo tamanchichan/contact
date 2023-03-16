eb66e706bdaaeffc755359f1f8070c42c4af427a
tamanchichan <tamanchichan@gmail.com>
2023-03-16 16:29:30 -0500

    remove div added

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,22 +4,19 @@ const input = document.querySelector('.input-field');
 const button = document.querySelector('.button');
 const contacts = document.querySelector('.contacts');
 
-function addContact() {
-  const box = document.createElement('div');
-  let contact = input.value
-  contacts.appendChild(box, contact).classList.add('box');
-  box.style.cursor = 'pointer';
-  box.innerText = input.value;
-}
-
 button.addEventListener('click', () => {
-  // function addContact() {
-  //   const box = document.createElement('div');
-  //   let contact = input.value
-  //   contacts.appendChild(box, contact).classList.add('box');
-  //   box.style.cursor = 'pointer';
-  //   box.innerText = input.value;
-  // }
+  const box = document.createElement('div');
+  
+  function addContact() {
+    let contact = input.value
+    contacts.appendChild(box, contact).classList.add('box');
+    box.style.cursor = 'pointer';
+    box.innerText = input.value;
+  }
+  
+  box.onclick = () => {
+    contacts.removeChild(box);
+  }
   
   addContact();
 })

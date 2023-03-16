9306f6cedfe01160ec0e0f2453197501dfd88698
tamanchichan <tamanchichan@gmail.com>
2023-03-16 15:42:30 -0500

    function created (create div)

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,3 +3,14 @@
 const input = document.querySelector('.input-field');
 const button = document.querySelector('.button');
 const contacts = document.querySelector('.contacts');
+
+button.addEventListener('click', () => {
+  function addContact() {
+    const box = document.createElement('div');
+    
+    contacts.appendChild(box).classList.add('box');
+    box.style.cursor = 'pointer';
+  }
+  
+  addContact();
+})

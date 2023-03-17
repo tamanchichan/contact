f1597cb6ba3c54cb16a3516636550492afc61a53
tamanchichan <tamanchichan@gmail.com>
2023-03-17 09:38:06 -0500

    index working

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,6 +4,9 @@ const input = document.querySelector('.input-field');
 const button = document.querySelector('.button');
 const contacts = document.querySelector('.contacts');
 
+const box = document.createElement('div');
+const p = document.createElement('p');
+
 class Contact {
   #name;
   #email;
@@ -14,21 +17,38 @@ class Contact {
     this.#email = email;
     this.#city = city;
   }
+  
+  set name(name) {this.#name};
+  get name() {return this.#name};
+  
+  set email(email) {this.#email};
+  get email() {return this.#email};
+  
+  set city(city) {this.#city};
+  get city() {return this.#city};
 }
 
+const contact = [];
+
 button.addEventListener('click', () => {
-  const box = document.createElement('div');
+  let value = input.value.split(',');
+    const newContact = new Contact(value[0], value[1], value[2]);
   
+  // create div
   function addContact() {
     contacts.appendChild(box, input.value).classList.add('box');
     box.style.cursor = 'pointer';
-    box.innerText = input.value;
+    box.appendChild(p).innerHTML = input.value;
   }
   
+  // remove div
   box.onclick = () => {
     contacts.removeChild(box);
   }
   
-  const array = [];
+  // add to array
+  contact.push(newContact);
+  console.log(contact);
+  
   addContact();
-})
+});
